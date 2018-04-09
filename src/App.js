23cc03cbe1251c9d9318e54916404f0f2428d471
keitheous <keithchongwy@gmail.com>
2018-04-09 12:59:01 +1000

    getting a hang of documents, very similar to angular!

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import Header from './components/headerComponent/header'
+import Footer from './components/footerComponent/footer'
+import HomePage from './components/pages/homePage'
+
+import './assets/css/default.min.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Test
+
+        <Header />
+
+        Body
+        <HomePage />
+        Ba ba Black sheep
+
+        <Footer />
       </div>
     );
   }
---
+++ src/components/pages/homePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class HomePage extends Component {
+  render() {
+    return (
+      <p> This is my body
+      </p>
+    );
+  }
+}
+
+export default HomePage;
